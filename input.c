fec34d8f88e1121ad2ea5896f41651e2d47855ff
MercylineOtwori <mercylinemoraaotwori@gmail.com>
2026-06-29 12:26:58 +0000

    Day 14 inputs

---
+++ input.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void) {
+	int age;
+	char name[50];
+
+	printf("Enter your name: ");
+	scanf("%s", name);
+
+	printf("Enter your age: ");
+	scanf("%d", &age);
+
+	printf("Hello, %s! You are %d years old.\n", name, age);
+
+	return 0;
+}
